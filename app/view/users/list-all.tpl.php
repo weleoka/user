6b138b178bc3355845b44e703ddc746bd251d92e
Kai Weeks <weleoka@github.com>
2014-12-18 16:46:34 +0100

    Fixed the template view a little

--- app/view/users/list-all.tpl.php
+++ app/view/users/list-all.tpl.php
@@ -4,50 +4,37 @@
 			//	unset($title);
 		}
 ?>
-<div class="commentAll">
-    <h4>
-        <?php  if (count($users) == 0) : ?>
-        Inga användare.
-        <?php elseif (count($users) == 1) : ?>
-        1 användare.
-        <?php else : ?>
-        <?php echo count($users); ?>
-        användare.
-        <?php endif; ?>
-    </h4>
+    <?php
+    if (isset($content)) {
+        echo $content;
+    }
+    ?>
 
-<?php $i = 0; foreach ($users as $user) : ?>
-		  		<?php if ($i % 2 == 0 ) : ?>
-        			<div class="commentUnit even">
-		  		<?php else : ?>
-		  			<div class="commentUnit odd">
-				<?php endif; $i++; ?>
+<?php $i = 0 ?>
+<?php foreach ($users as $user) : ?>
+		  <?php if ($i % 2 == 0 ) : ?>
+        <div class="userUnit even">
+		  <?php else : ?>        
+		  <div class="userUnit odd">
+		  <?php endif; $i++; ?>
+<?php if($user->active === null) {$status = 'Inaktiv';} else {$status = 'Aktiv';} ?>
+<?php if(isset($user->deleted)) {$status = $status . ' och i papperskorgen';} else { }?>
 
-            <div class="commentBox">
-					<?php if($user->active === null) {$status = 'Inaktiv';} else {$status = 'Aktiv';} ?>
-					<?php if(isset($user->deleted)) {$status = $status . ' och i papperskorgen';} else { }?>
+   <img style="float: left" src="http://www.gravatar.com/avatar/<?=md5($user->email);?>.jpg?s=60">
+	&nbsp&nbspID# <?=$user->id?>: <a href='<?=$this->url->create('users/id/' . $user->id)?>'><?=$user->acronym?></a> ( <?=$status?> )<br>
+	<?php if (isset($admin)) : ?>&nbsp&nbspEmail: <?=$user->email?><br><?php endif; ?>
+	&nbsp&nbspNamn: <?=$user->name?><br>
+	Skapad: <?=$user->created?>
 
-               <div class="gravatar">
-  						<img src="http://www.gravatar.com/avatar/<?=md5($user->email);?>.jpg?s=60">
-               </div>
-               <div class="commentData">
-						ID# <?=$user->id?>:
-						<a href='<?=$this->url->create('users/id/' . $user->id)?>'><?=$user->acronym?></a> ( <?=$status?> )<br>
-						<?php if (isset($admin)) : ?>Email: <?=$user->email?><br><?php endif; ?>
-						Namn: <?=$user->name?><br>
-						Skapad: <?=$user->created?>
-					</div>
-					<p class="commentContent">
-					</p>
-					<?php if (isset($admin)) : ?>
-	            	<div class="commentButtonsDiv">
-							<a href='<?=$this->url->create('users/delete/' . $user->id)?>'>Radera</a>&nbsp&nbsp&nbsp
-							<a href='<?=$this->url->create('users/update/' . $user->id)?>'>Uppdatera</a>&nbsp&nbsp&nbsp
-							<a href='<?=$this->url->create('users/softDelete/' . $user->id)?>'>Ta bort/Aterställ</a>&nbsp&nbsp&nbsp
-							<a href='<?=$this->url->create('users/changeStatus/' . $user->id)?>'>Inaktivera/Aktivera</a>
-						</div>
-					<?php endif; ?>
-				</div>	
-			</div>
-<?php endforeach; ?>
+		<?php if (isset($admin)) : ?>
+
+	<p>
+		<a href='<?=$this->url->create('users/delete/' . $user->id)?>'>Radera</a>&nbsp&nbsp&nbsp
+		<a href='<?=$this->url->create('users/update/' . $user->id)?>'>Uppdatera</a>&nbsp&nbsp&nbsp
+		<a href='<?=$this->url->create('users/softDelete/' . $user->id)?>'>Ta bort/Aterställ</a>&nbsp&nbsp&nbsp
+		<a href='<?=$this->url->create('users/changeStatus/' . $user->id)?>'>Inaktivera/Aktivera</a></p>
+	<hr>
+
+		<?php endif; ?>
 </div>
+<?php endforeach; ?>
